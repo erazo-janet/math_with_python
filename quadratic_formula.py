29db651a788fd4cfab448c18f81ea5b3bb97907a
Janet Chavez Erazo <76828004+erazo-janet@users.noreply.github.com>
2021-01-04 13:35:35 -0800

    Update quadratic_formula.py

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -9,7 +9,7 @@ a = int(input("Enter a number for a: "))
 b = int(input("Enter a number for b: "))
 c = int(input("Enter a number for c: "))
 
-#calculate the discriminant to determine how many solutions for x there will be
+#calculate the discriminant to determine how many solutions there will be for x
 disc = b**2 - 4*a*c
 
 #checking discriminant to find x
